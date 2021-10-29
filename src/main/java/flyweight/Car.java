6a4a21d28407657ea55efd14ebb992527ed98262
Vasyl Bidyak <v.bidyak@asd-team.com>
2021-10-29 06:46:47 +0300

    change flyweight pattern

---
+++ src/main/java/flyweight/Car.java
@@ -0,0 +1,18 @@
+package flyweight;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Car {
+	private String name;
+	private CarType carType;
+	private Integer maxSpeed;
+
+	public Car(String name, String carBody, Integer numOfDoors, Integer maxSpeed) {
+		this.name = name;
+		carType = CarFactory.getCarType(carBody, numOfDoors);
+		this.maxSpeed = maxSpeed;
+	}
+}
